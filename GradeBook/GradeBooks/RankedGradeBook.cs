68089e391b40fb60394d436361597062d0031732
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-26 22:40:28 -0300

    Fixed the exception requirement, I don't even know why it didn't work before...
    Added some info on the summary about the other two methods added.
    Also added formatting.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,9 +19,20 @@ namespace GradeBook.GradeBooks
 
         #region Methods
 
+        /// <summary>
+        /// Creates a descending list ordered by average grade.
+        /// <br></br>
+        /// Calculates the number that represents twenty percent.
+        /// <br></br>
+        /// Verifies if the average grade passed on the method is higher or equal than the bottom 20% of the ranked list.
+        /// </summary>
+        /// <param name="averageGrade">Grade to be evaluated by this method.</param>
+        /// <returns>The grade as a letter.</returns>
         public override char GetLetterGrade(double averageGrade)
         {
-            // Implementation
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
             List<Student> rankedList = GenerateRankedStudentList();
             int twentySplit = CalculateTwentyPercent(rankedList);
 
@@ -37,6 +48,10 @@ namespace GradeBook.GradeBooks
                 return 'F';
         }
 
+        /// <summary>
+        /// Generates an ordered list by Average Grade ( Descending Order )
+        /// </summary>
+        /// <returns></returns>
         public List<Student> GenerateRankedStudentList()
         {
             List<Student> rankedStudentList = new List<Student>();
@@ -49,6 +64,11 @@ namespace GradeBook.GradeBooks
             return rankedStudentList;
         }
 
+        /// <summary>
+        /// Returns a number that represents 20% of the students count.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
         public int CalculateTwentyPercent(List<Student> list)
         {
             return (list.Count / 5);
